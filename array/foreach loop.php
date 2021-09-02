9111b81797edfcc079f7ae4f904449432a288ca5
aditya8100-hstu <58524511+aditya8100-hstu@users.noreply.github.com>
2021-09-02 10:08:37 +0600

    Add files via upload

---
+++ array/foreach loop.php
@@ -0,0 +1,17 @@
+<?php
+
+$a = [1,2,3,4,5,6,7,8];
+
+foreach($a as $value){
+	echo $value. "<br>" ;
+}
+
+$b = array(
+      "c"=>10,
+      "d"=>20
+      );
+ foreach($b as $key => $value){
+ echo $key. " = ". $value. "<br>";
+ }
+
+?>
