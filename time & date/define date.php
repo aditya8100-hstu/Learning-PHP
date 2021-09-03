8e121272c6949c2d1fdcf7f85005a171e871c55d
aditya8100-hstu <58524511+aditya8100-hstu@users.noreply.github.com>
2021-09-04 00:46:20 +0600

    Add files via upload

---
+++ time & date/define date.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Today date: " .date("d")."<br>" ;
+echo "Today date: " .date("j")."<br>" ;
+echo "Today date: " .date("jS")."<br><br>" ;
+
+echo "Today Month: ".date("F")."<br>" ;
+echo "Today Month: ".date("m")."<br>" ;
+echo "Today Month: ".date("M")."<br>" ;
+echo "Today Month: ".date("n")."<br><br>" ;
+
+echo "Today Year: ".date("Y")."<br>" ;
+echo "Today Year: ".date("y")."<br><br>" ;
+
+echo "Today is: ".date("D")."<br>" ;
+echo "Today is: ".date("l")."<br>" ;
+echo "Today is: ".date("N")."<br>" ;
+echo "Today is: ".date("w")."<br><br>" ;
+
+echo "Full Date: ". date("jS-F-Y , l") ;
+
+
+
+
+
+?>
---
+++ time & date/define time.php
@@ -0,0 +1,14 @@
+<?php
+
+echo"The time is: " .date("h-i-sa , e")."<br><br>";
+
+
+
+date_default_timezone_set("Asia/Dhaka");
+echo"The time is: " .date("h-i-sa , e")."<br><br>";
+echo"The time is: " .date("H-i-sa , e")."<br><br>";
+echo"The time is: " .date("g-i-sa , e")."<br><br>";
+echo"The time is: " .date("G-i-sa , e")."<br><br>";
+
+
+?>
